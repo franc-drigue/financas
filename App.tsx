da91d99996066981d5d3cfaf390225203d35c4c2
Francisco <francdrigue@gmail.com>
2025-04-18 15:51:13 -0300

    adjustment

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import 'react-native-gesture-handler';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import RouterIndex from './src/routes';
 
 export default function App (){
-  return(
-    <NavigationContainer>
-       <StatusBar/>
-    </NavigationContainer>
-  )
+  return <RouterIndex/>
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import StackAuth from './auth.route';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 
 export default function RouterIndex() {
+ const loading = false
+ const SignIn = false
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <NavigationContainer>
+       <StatusBar/>
+       {
+         SignIn ? 
+         <View></View> 
+         : 
+         <StackAuth/>
+       }
+    </NavigationContainer>
   )
 }
